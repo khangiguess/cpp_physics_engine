4b51398e70304d6761c94697965cd9c4bcb56f88
khangiguess <khangtrinh007@gmail.com>
2026-05-07 22:17:03 +0700

    Fay, please remind me to go over collision.h again cuz im deadass dont fully understand the syntax

--- collision.h
+++ collision.h
@@ -7,7 +7,15 @@ vec2D distanceVec(const vec2D& a, const vec2D& b){
     return vec2D(a.x - b.x, a.y - b.y);
 }
 
-bool isCollided(const shape& a, const shape& b){
+bool isCollided(const shape& a, const shape& b, 
+    const vec2D& posA, const vec2D& posB){
+        if(a.type == shapeType::CIRCLE && b.type == shapeType::CIRCLE){
+            const circle& circA = static_cast<const circle&>(a);
+            const circle& circB = static_cast<const circle&>(b);
+            vec2D distVec = distanceVec(posA, posB);
+            float dist = distVec.magnitude();
+            return dist < (circA.radius + circB.radius);
+        }
     
 }
 
